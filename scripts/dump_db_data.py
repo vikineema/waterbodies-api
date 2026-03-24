4c749c89af6f9b7ad313fafe0526961d764bdc1e
Victoria Neema <victoria@kartoza.com>
2026-03-24 09:05:20 +0300

    Add percentiles setup

--- scripts/dump_db_data.py
+++ scripts/dump_db_data.py
@@ -10,6 +10,7 @@ from sqlalchemy.event import listen
 from water_quality.logs import setup_logging
 from water_quality.summaries.summary import (
     add_water_quality_observations_to_db,
+    add_water_quality_percentiles_to_db
 )
 from waterbodies.db import get_waterbodies_engine, get_test_waterbodies_engine
 from waterbodies.db_models import WaterbodyBase
@@ -40,12 +41,16 @@ file_db_engine = get_test_waterbodies_engine()
 _log.info(f"File-based SQLite database  db url: {file_db_engine.url}")
 
 # Add historical extent sample data
-sample_waterbody_observations = gpd.read_file(file_db_path, layer="waterbodies_historical_extent")
-add_waterbodies_polygons_to_db(sample_waterbody_observations, engine)
+sample_waterbodies = gpd.read_file(file_db_path, layer="waterbodies_historical_extent")
+add_waterbodies_polygons_to_db(sample_waterbodies, engine)
 
 # Add surface area change sample data
 create_waterbodies_observations_table(engine)
 
-# Add water quality sample data
+# Add water quality summaries sample data
 sample_water_quality_observations = pd.read_sql("SELECT * FROM waterbodies_water_quality ", con=file_db_engine)
 add_water_quality_observations_to_db(sample_water_quality_observations, engine)
+
+# Add water quality percentiles sample data
+sample_waterbody_percentiles = pd.read_sql("SELECT * FROM waterbodies_water_quality_percentiles", con=file_db_engine)
+add_water_quality_percentiles_to_db(sample_waterbody_percentiles, engine)
